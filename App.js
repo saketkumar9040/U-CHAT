f40121c0522b20af046336606dc3cce6e2852bc8
saket kumar <saketk89@gmail.com>
2023-06-13 19:06:09 +0530

    feat - added androidStatusBar and androidNavigaionBar in app.json

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as NavigationBar from "expo-navigation-bar";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from "react";
+
+SplashScreen.preventAutoHideAsync()
+NavigationBar.setBackgroundColorAsync("#6F4E37");
 
 export default function App() {
+
+  const [appLoaded, setAppLoaded] = useState(false);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+        setAppLoaded(true);
+    },2000)
+  },[]);
+
+  const onLayout = useCallback(async()=>{
+    if(appLoaded){
+     await SplashScreen.hideAsync();
+    }
+  },[appLoaded]);
+
+  if(!appLoaded){
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container} onLayout={onLayout}>
+      <SafeAreaView>
+        <StatusBar style="light" backgroundColor="#6F4E37" barStyle="light-content"/>
+        <Text>Hello U-Chat</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#ffbf00",
     alignItems: 'center',
     justifyContent: 'center',
   },
